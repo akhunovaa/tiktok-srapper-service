ee8b092975dfe993cc892d51af6b0a9b89e538e7
Azat Akhunov <azat.akhunov@protonmail.com>
2020-11-10 17:25:41 +0300

    project init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {app} from './app'
 import {AddressInfo} from 'net'
-const server = app.listen(7321, '127.0.0.1', () => {
+const server = app.listen(7321, '0.0.0.0', () => {
     const {port, address} = server.address() as AddressInfo;
     console.log('Server listening on:','http://' + address + ':'+port);
 });
