63e9fca40fbc3b371637dc166af50cdb228023dd
Azat Akhunov <azat.akhunov@protonmail.com>
2020-11-11 11:01:47 +0300

    project init

--- src/app.ts
+++ src/app.ts
@@ -12,49 +12,55 @@ app.use(bodyParser.json({
 }));
 
 app.get('/hashtag/:hashtag/:cursor/:water', async (req, res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const tagValue = req.params.hashtag
     const cursor = req.params.cursor as unknown as number
     const water = req.params.water as unknown as boolean
-    const posts = await hashtag(tagValue, { number: cursor, noWaterMark: water, proxy: '82.81.169.142:80' });
+    const posts = await hashtag(tagValue, { number: cursor, noWaterMark: water, proxy: proxyList });
     console.log(posts);
     res.send(posts);
 });
 
 app.get('/hashtag/:hashtag', async (req, res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const tagValue = req.params.hashtag
-    const tag = await getHashtagInfo(tagValue, {proxy: '82.81.169.142:80'});
+    const tag = await getHashtagInfo(tagValue, {proxy: proxyList});
     console.log(tag);
     res.send(tag);
 });
 
 app.get('/user/:user/:count/:byUserId', async (req, res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const username = req.params.username
     const count = req.params.count as unknown as number
     const byUserId = req.params.water as unknown as boolean
-    const posts = await user(username, { number: count, by_user_id: byUserId, proxy: '82.81.169.142:80' });
+    const posts = await user(username, { number: count, by_user_id: byUserId, proxy: proxyList });
     console.log(posts);
     res.send(posts);
 });
 
 app.get('/trend/:count', async (req, res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const count = req.params.count as unknown as number
-    const posts = await trend('', { number: count, proxy: '82.81.169.142:80' });
+    const posts = await trend('', { number: count, proxy: proxyList });
     console.log(posts);
     res.send(posts);
 });
 
 app.get('/profile/:username', async (req, res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const username = req.params.username
-    const userInfo = await getUserProfileInfo(username, {proxy: '82.81.169.142:80'});
+    const userInfo = await getUserProfileInfo(username, {proxy: proxyList});
     console.log(userInfo);
     res.send(userInfo);
 });
 
 app.post('/video', async (req,res) => {
+    const proxyList: string[] = ['189.170.79.21:8080', '123.101.207.33:9999', '159.8.114.34:8123'];
     const videoUrl = req.body.videoUrl;
     console.log(videoUrl);
     try {
-        const videoMeta = await getVideoMeta(videoUrl, {proxy: '82.81.169.142:80'});
+        const videoMeta = await getVideoMeta(videoUrl, {proxy: proxyList});
         console.log(videoMeta);
         res.send(videoMeta);
     } catch (error) {
