96aeaaf0a5bacaa201d0eac5a0115d3bcb1d657f
Azat Akhunov <azat.akhunov@protonmail.com>
2020-11-12 20:24:46 +0300

    project init

--- src/app.ts
+++ src/app.ts
@@ -16,17 +16,27 @@ app.get('/hashtag/:hashtag/:cursor/:water', async (req, res) => {
     const tagValue = req.params.hashtag
     const cursor = req.params.cursor as unknown as number
     const water = req.params.water as unknown as boolean
-    const posts = await hashtag(tagValue, { number: cursor, noWaterMark: true, proxy: proxyList });
-    console.log(posts);
-    res.send(posts);
+    try {
+        const posts = await hashtag(tagValue, { number: cursor, noWaterMark: true, proxy: proxyList });
+        console.log(posts);
+        res.send(posts);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.get('/hashtag/:hashtag', async (req, res) => {
     const proxyList: string[] = ['C6sSbU:zWeGcu@45.132.20.183:8000', 'C6sSbU:zWeGcu@45.132.22.155:8000', 'EWKspn:mXKd86@194.242.124.40:8000', 'q29LDc:vwmFqk@194.242.125.1:8000', 'q29LDc:vwmFqk@194.242.125.105:8000'];
     const tagValue = req.params.hashtag
-    const tag = await getHashtagInfo(tagValue, {proxy: proxyList});
-    console.log(tag);
-    res.send(tag);
+    try {
+        const tag = await getHashtagInfo(tagValue, {proxy: proxyList});
+        console.log(tag);
+        res.send(tag);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.get('/user/:user/:count/:byUserId', async (req, res) => {
@@ -34,34 +44,53 @@ app.get('/user/:user/:count/:byUserId', async (req, res) => {
     const username = req.params.username
     const count = req.params.count as unknown as number
     const byUserId = req.params.water as unknown as boolean
-    const posts = await user(username, {number: count, by_user_id: byUserId, proxy: proxyList});
-    console.log(posts);
-    res.send(posts);
+    try {
+        const posts = await user(username, {number: count, by_user_id: byUserId, proxy: proxyList});
+        console.log(posts);
+        res.send(posts);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.get('/trend/:count', async (req, res) => {
     const proxyList: string[] = ['C6sSbU:zWeGcu@45.132.20.183:8000', 'C6sSbU:zWeGcu@45.132.22.155:8000', 'EWKspn:mXKd86@194.242.124.40:8000', 'q29LDc:vwmFqk@194.242.125.1:8000', 'q29LDc:vwmFqk@194.242.125.105:8000'];
     const count = req.params.count as unknown as number
-    const posts = await trend('', {number: count, proxy: proxyList});
-    console.log(posts);
-    res.send(posts);
+    try {
+        const posts = await trend('', {number: count, proxy: proxyList});
+        console.log(posts);
+        res.send(posts);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.get('/profile/:username', async (req, res) => {
     const proxyList: string[] = ['C6sSbU:zWeGcu@45.132.20.183:8000', 'C6sSbU:zWeGcu@45.132.22.155:8000', 'EWKspn:mXKd86@194.242.124.40:8000', 'q29LDc:vwmFqk@194.242.125.1:8000', 'q29LDc:vwmFqk@194.242.125.105:8000'];
     const username = req.params.username
-    const userInfo = await getUserProfileInfo(username, {proxy: proxyList});
-    console.log(userInfo);
-    res.send(userInfo);
+    try {
+        const userInfo = await getUserProfileInfo(username, {proxy: proxyList});
+        console.log(userInfo);
+        res.send(userInfo);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.post('/video', async (req, res) => {
     const proxyList: string[] = ['C6sSbU:zWeGcu@45.132.20.183:8000', 'C6sSbU:zWeGcu@45.132.22.155:8000', 'EWKspn:mXKd86@194.242.124.40:8000', 'q29LDc:vwmFqk@194.242.125.1:8000', 'q29LDc:vwmFqk@194.242.125.105:8000'];
     const videoUrl = req.body.videoUrl;
-    console.log(videoUrl);
-    const videoMeta = await getVideoMeta(videoUrl, {proxy: proxyList, noWaterMark: true});
-    console.log(videoMeta);
-    res.send(videoMeta);
+    try {
+        const videoMeta = await getVideoMeta(videoUrl, {proxy: proxyList, noWaterMark: true});
+        console.log(videoMeta);
+        res.send(videoMeta);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 
