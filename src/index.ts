31958c8e113423de3f0ce95d487b353e3edc2d20
Azat Akhunov <azat.akhunov@protonmail.com>
2020-11-25 13:09:11 +0300

    music scrapper added

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {app} from './app'
 import {AddressInfo} from 'net'
 const server = app.listen(7321, '0.0.0.0', () => {
     const {port, address} = server.address() as AddressInfo;
-    console.log('Server listening on:','http://' + address + ':'+port);
+    console.log('Server  listening on:','http://' + address + ':'+port);
 });
