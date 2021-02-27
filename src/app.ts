05e7898b68faf7dbfa27cd7b622b1c8fe3873d7d
Azat Akhunov <azat.akhunov@protonmail.com>
2021-02-28 00:15:05 +0300

    обновлена билбиотека скраппера

--- src/app.ts
+++ src/app.ts
@@ -144,7 +144,7 @@ app.get('/music/:musicId', async (req, res) => {
     const proxyList: string[] = ['05JAsv:dLW40U@194.62.30.31:8000', 'C6sSbU:zWeGcu@45.132.20.183:8000', 'C6sSbU:zWeGcu@45.132.22.155:8000', 'EWKspn:mXKd86@194.242.124.40:8000', 'q29LDc:vwmFqk@194.242.125.1:8000', 'q29LDc:vwmFqk@194.242.125.105:8000'];
     const musicUrl = 'https://www.tiktok.com/music/original-sound-' + req.params.musicId
     try {
-        const musicPost = await getMusicInfo(musicUrl, { proxy: proxyList});
+        const musicPost = await getMusicInfo(musicUrl, { });
         console.log(musicPost);
         res.send(musicPost);
     } catch (error) {
